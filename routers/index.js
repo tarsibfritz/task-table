0efe7e332cc57ec8e18ed91773e9e317bba062f7
Tarsila <tarsilabfritz0305@outlook.com>
2024-04-04 19:41:03 -0300

    alteração no router

--- routers/index.js
+++ routers/index.js
@@ -5,5 +5,5 @@ const routerTasks = require("./taskRouters");
 module.exports = function (app, express) {
     app.use(express.json());  // Defini um middleware do Express (express.json()) para analisar dados de requisição no formato JSON
     app.use(express.urlencoded({ extended: true }));  // Define um middleware do Express (express.urlencoded()) para analisar dados de requisição de formulário HTML e torná-los acessíveis no objeto req.body do Express
-    app.use(routerTasks);  // 
+    app.use(routerTasks);  
 };
